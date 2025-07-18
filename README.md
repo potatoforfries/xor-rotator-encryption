d9e83dcaab52291863004dcb413460b903e2d8cb
ycu2yr2p8@gmail.com <youremail@example.com>
2025-07-18 10:44:43 +0530

    wrote a  simple readme.md and improved the code with comment

--- README.md
+++ README.md
@@ -1,1 +1,45 @@
-# xor-rotator-encryption
+# Simple One Time Pad
+
+a python class with simple implementation of One Time Pad encryption using bitwise XOR operations.
+
+## Usage
+```python
+import OneTimePad 
+
+# create the class object and insert the secret
+# text to generate a random key of the same length as your text
+otp = OneTimePad('secret text!') 
+
+# returns an encrypted list of your text
+encrypted = otp.encrypt('secret text!')
+
+# returns a decrypted text again
+decrypted = otp.decrypt(encrypted)
+```
+### OR
+
+```python
+import OneTimePad
+
+# random key input by user
+Key = 1231       #has to be an integer, byte or bit
+
+# no need to mention text to encrypt in the class object
+# parameter as its only used to generate random key
+otp = OneTimePad(key=Key)  
+
+# returns an encrypted list of your text using your custom Key
+encrypted = otp.encrypt('secret text!')
+
+# returns a decrypted text again
+decrypted = otp.decrypt(encrypted)
+```
+please check the code to see how random bytes are generated and how the function works for custom usage. 
+
+## Contributing
+
+Pull requests are welcome. This is just a hobby project which I plan to slowly expand. 
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
--- main.py
+++ main.py
@@ -1,2 +1,47 @@
-def xorRotator(key:int, value:str):
-    pass
+import os
+
+class OneTimePad:
+    def __init__(self, text:str = None, key:int = None):   
+
+        # Either a key or text has to be provided
+        # if both text and key are provided, the text is not used to generate a key, rather the provided key is used
+        if not (text or key):
+            raise ValueError("either text or key has to be provided!!")
+        elif key:               
+            self.key = key.to_bytes(len(str(key)), byteorder = 'big')   #'big' -> converts int to big endian byte
+        else:               
+            # if text is only provided, generates a truly random key, same length as the text
+            self.key = os.urandom(len(text))    # os.urandom() returns a byte object with random bytes
+
+    def encrypt(self, text:str):
+        encrypted = []
+        # convert each character in the text and key into integers and XOR them
+        for char, k_byte in zip(text, self.key):
+            char_ascii = ord(char)          # Convert character to ASCII integer
+            # Encrypt the text using XOR
+            enc_val = char_ascii ^ k_byte   # k_byte already an integer
+            encrypted.append(enc_val) 
+        
+        return encrypted
+    
+    def decrypt(self, encrypted):
+        decrypted = ""
+        # Decrypt the text by XORing the encrypted values with the same key
+        # XORing again with the same key gives back the original character
+        for enc_val, k_byte in zip(encrypted, self.key):
+            decrypted_ascii = enc_val ^ k_byte 
+            decrypted += chr(decrypted_ascii)    # Join all decrypted characters into a string
+
+        return decrypted
+
+
+if __name__ == "__main__":
+    x = OneTimePad("aStringwith20letters")
+    print("key in byte(s): ", x.key)
+    print(len("Exaaaaactly20letters"))
+    print(len("aStringwith20letters"))
+    enc = x.encrypt("Exaaaaactly20letters")
+    print("\nencrypted list of integers: ", enc)
+
+    dec = x.decrypt(enc)
+    print("\ndecrypted string: ", dec)
